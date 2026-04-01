40f019b3537e440a103f465afb88aaeb159a98ef
Ignas <15948796+ignasc@users.noreply.github.com>
2026-04-01 17:39:34 +0300

    Add prettier package and basic web app components.

---
+++ src/components/CardBoard.jsx
@@ -0,0 +1,16 @@
+import Card from './Card.jsx'
+
+function CardBoard(){
+  return (
+  <ul>
+    <li>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      </li>
+   </ul>
+  )
+}
+
+export default CardBoard;
---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,7 @@
+function ScoreBoard(){
+  return (
+    <div>ScoreBoard component</div>
+  )
+}
+
+export default ScoreBoard;
