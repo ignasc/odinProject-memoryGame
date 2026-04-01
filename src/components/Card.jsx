132a1dc9171c91a62f8e66d7eab4023e9e61597f
Ignas <15948796+ignasc@users.noreply.github.com>
2026-04-01 18:18:06 +0300

    Add an image to the card component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,11 @@
-function Card() {
-  return <div>card item</div>;
+import img_noImage from '../assets/no-image-icon-23494.png';
+function Card({ imgSrc = null }) {
+  return (
+    <div>
+      <img src={imgSrc ? imgSrc : img_noImage} alt="no image found" />
+      <p>Card component</p>
+    </div>
+  );
 }
 
 export default Card;
