236fa3395f18ef72cfa1ac5747c3d27b0759fd58
Ignas <15948796+ignasc@users.noreply.github.com>
2026-04-02 16:29:56 +0300

    Add css template from previous project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import './css/resetcss.css';
+import './css/darkAndLightThemes.css';
+import './css/styles.css';
+
 import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
-import './App.css';
 import ScoreBoard from './components/ScoreBoard.jsx';
 import CardBoard from './components/CardBoard.jsx';
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+
+  const [themeClass, setThemeClass] = useState('light');
+  document.body.classList.add('light');
 
   function incrementScore(setValue) {
     if (setValue === 0) {
